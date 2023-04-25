90bbea96e76234e8f00db99fcc93a2bc61febe43
pvc97 <cuongpv2209@gmail.com>
2023-04-25 23:25:22 +0700

    Add tag to jwt claims.

--- model/claims.go
+++ model/claims.go
@@ -4,7 +4,7 @@ import "github.com/golang-jwt/jwt"
 
 // Claims means the data in the token
 type JwtCustomClaims struct {
-	UserId string
-	Role   string
+	UserId string `json:"userId"`
+	Role   string `json:"role"`
 	jwt.StandardClaims
 }
--- router/api.go
+++ router/api.go
@@ -13,7 +13,7 @@ type API struct {
 }
 
 func (api *API) SetupRouter() {
-	api.Echo.POST("/user/sign-in", api.UserHandler.HandleSignIn, mid.IsAdmin())
+	api.Echo.POST("/user/sign-in", api.UserHandler.HandleSignIn)
 	api.Echo.POST("/user/sign-up", api.UserHandler.HandleSignUp)
 	api.Echo.POST("/user/profile", api.UserHandler.Profile, mid.JwtMiddleware())
 }
